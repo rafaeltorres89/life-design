120116e17a9c0ae8d865a5684f3e0a543cd8b3c8
Rafael Feitoza Torres <rafael.feitoza@tivit.com>
2018-07-12 22:50:41 -0300

    New files for static pages

---
+++ src/pages/about-us/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import '../../dist/css/mongeral.min.css';
+
+class AboutUs extends Component {
+  render() {
+    return (
+      <div className="content">
+      </div>
+    );
+  }
+}
+
+export default AboutUs;
---
+++ src/pages/installation/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import '../../dist/css/mongeral.min.css';
+
+class Installation extends Component {
+  render() {
+    return (
+      <div className="content">
+      </div>
+    );
+  }
+}
+
+export default Installation;
