6fa5b2a879549b7e388a8156f1ad5e37ff277d48
Rafael Feitoza Torres <rafael.feitoza@tivit.com>
2018-07-13 15:51:06 -0300

    Navbar vendor github

--- src/include/header/index.js
+++ src/include/header/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Navbar from '../navbar';
+import VendorBar from '../vendorbar';
 
 const Header = () => {
     return (
         <header>
             <h1>Header do site</h1>
             <Navbar />
+            <VendorBar />
         </header>
     );
 }
---
+++ src/include/vendorbar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const VendorBar = () => {
+    return (
+        <ul>
+            <li>a</li>
+            <li>b</li>
+            <li>c</li>
+        </ul>
+    );
+}
+
+export default VendorBar;
