24a2788fd9e1de9e8a94b9674cc17fa32b997f35
Rafael Feitoza Torres <rafael.feitoza@tivit.com>
2018-07-18 15:01:31 -0300

    Navbar direito ancor

--- src/include/aside/index.js
+++ src/include/aside/index.js
@@ -5,6 +5,12 @@ const Aside = () => {
         <main>
             <div className="skippy">
             </div>
+            <ul className="section-nav">
+                <li><a href="" title="" className="lk">List 1</a></li>
+                <li><a href="" title="" className="lk">List 2</a></li>
+                <li><a href="" title="" className="lk">List 3</a></li>
+                <li><a href="" title="" className="lk">List 4</a></li>
+            </ul>
         </main>
     );
 }
--- src/include/vendorbar/index.js
+++ src/include/vendorbar/index.js
@@ -4,7 +4,7 @@ const VendorBar = () => {
     return (
         <ul className="vendorbar">
             <li>v0.1</li>
-            <li><a href="" title="Github"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://github.com/rafaeltorres89/life-design" title="Github"><i class="fab fa-github"></i></a></li>
             <li><a href="" title="Slack"><i class="fab fa-slack"></i></a></li>
         </ul>
     );
