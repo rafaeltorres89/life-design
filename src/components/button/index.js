ca39e6143313c808b334f130cad02b360780fbba
Rafael Feitoza Torres <rafael.feitoza@tivit.com>
2018-07-18 16:32:35 -0300

    Create page buttom

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -4,6 +4,8 @@ const Button = () => {
     return (
         <div className="pages">
             <h2>Button</h2>
+            <p className="info-page">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <h3>Examples</h3>
         </div>
     );
 }
