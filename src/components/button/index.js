5644e318dfe99462fd9326f60ceae3916f13c2eb
Rafael Feitoza Torres <rafael.feitoza@tivit.com>
2018-07-18 17:26:08 -0300

    Definição color  principais

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -6,6 +6,19 @@ const Button = () => {
             <h2>Button</h2>
             <p className="info-page">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             <h3>Examples</h3>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+            <div className="bd-example flex-container">
+                <button type="button" class="btn btn-primary">Primary</button>
+                <button type="button" class="btn btn-secondary">Secondary</button>
+                <button type="button" class="btn btn-success">Success</button>
+                <button type="button" class="btn btn-danger">Danger</button>
+                <button type="button" class="btn btn-warning">Warning</button>
+                <button type="button" class="btn btn-info">Info</button>
+                <button type="button" class="btn btn-light">Light</button>
+                <button type="button" class="btn btn-dark">Dark</button>
+
+                <button type="button" class="btn btn-link">Link</button>
+            </div>
         </div>
     );
 }
