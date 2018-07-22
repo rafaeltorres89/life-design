4612fa8ab648bbfa4f9c75f490ef9bed10774dc0
Rafael Feitoza Torres <rafael.feitoza@tivit.com>
2018-07-22 00:38:02 -0300

    Inclusão breadcrumb page checkout

--- src/components/checkbox/index.js
+++ src/components/checkbox/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import Breadcrumb from '../../include/breadcrumb';
+
 const Checkbox = () => {
     return (
         <div className="components">
+            <Breadcrumb />
             <h2>Checkbox</h2>
         </div>
     );
