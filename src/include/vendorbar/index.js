f73fc0916f7c9fdb656d53eda085802e1db76ad8
Rafael Feitoza Torres <rafael.feitoza@tivit.com>
2018-07-22 19:59:51 -0300

    Link teams mongeral

--- src/include/vendorbar/index.js
+++ src/include/vendorbar/index.js
@@ -5,7 +5,7 @@ const VendorBar = () => {
         <ul className="vendorbar">
             <li>v0.1</li>
             <li><a href="https://github.com/rafaeltorres89/life-design" title="Github"><i class="fab fa-github"></i></a></li>
-            <li><a href="" title="Slack"><i class="fab fa-slack"></i></a></li>
+            <li><a href="https://teams.microsoft.com/_?lm=deeplink&lmsrc=officeWaffle#/conversations/General?threadId=19:e0e588b2cc844b658f0a9e10cd56e16d@thread.skype&ctx=channel" title="Teams"><i class="fab fa-slack"></i></a></li>
         </ul>
     );
 }
